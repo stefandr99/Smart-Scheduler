52b214d4348594bbe4729aed3381dd7b0d280174
Stefan Dragoi <stefan.dragoi@endava.com>
2021-11-02 10:46:21 +0200

    added calendar entry bean

---
+++ SmartScheduler/src/main/java/master/aset/smartscheduler/managedBeans/CalendarEntry.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package master.aset.smartscheduler.managedBeans;
+
+import java.io.Serializable;
+import java.sql.Date;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Stefan
+ */
+@Named(value = "calendarEntry")
+@SessionScoped
+public class CalendarEntry implements Serializable{
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the startDate
+     */
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * @param startDate the startDate to set
+     */
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    /**
+     * @return the finishDate
+     */
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    /**
+     * @param finishDate the finishDate to set
+     */
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+    private String name;
+
+    private Date startDate;
+
+    private Date finishDate;
+}
