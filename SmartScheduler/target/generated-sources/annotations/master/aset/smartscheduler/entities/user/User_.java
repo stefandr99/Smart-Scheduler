c98bac1aa6a2a7e7dda4a6aee29e900f85d5043e
Damian Alexandru <damian.alexandru99@gmail.com>
2021-11-07 17:58:24 +0200

    added the UI component for calendar display and moved from java 7 to 8

--- SmartScheduler/target/generated-sources/annotations/master/aset/smartscheduler/entities/user/User_.java
+++ SmartScheduler/target/generated-sources/annotations/master/aset/smartscheduler/entities/user/User_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-11-01T23:33:39")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-11-07T17:45:19")
 @StaticMetamodel(User.class)
 public class User_ { 
 
